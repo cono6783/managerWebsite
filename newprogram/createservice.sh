04ada2b0e75e7394e380157b0486c017124e0e40
cono6783 <86134894+cono6783@users.noreply.github.com>
2025-08-05 00:29:22 -0500

    reload the systemctl daemon before starting the serivce

--- newprogram/createservice.sh
+++ newprogram/createservice.sh
@@ -14,6 +14,7 @@ echo "" >> /etc/systemd/system/$1.service
 echo "[Install]" >> /etc/systemd/system/$1.service
 echo "WantedBy=multi.user.target" >> /etc/systemd/system/$1.service
 
+systemctl daemon-reload
 systemctl start $1 #This will get moved to the project management page in the future
 systemctl enable $1 #Enabling the service to run on startup might be moved to an option
 
